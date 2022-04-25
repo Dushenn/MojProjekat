912bd8e3ebec7d3f12b92609402ccb04b625c4e0
Dusan <dupopovic04@gmail.com>
2022-04-25 20:22:31 +0200

    Dodavanje programa

---
+++ ProgramNeki/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramNeki
+{
+    class Osoba
+    {
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string jmbg { get; set; }
+        public int godiste { get; set; }
+
+        public Osoba(string im, string pre, string j, int god)
+        {
+            ime = im;
+            prezime = pre;
+            jmbg = j;
+            godiste = god;
+        }
+        public int Godine()
+        {
+            return DateTime.Today.Year - godiste;
+        }
+
+        public override string ToString()
+        {
+            return ime + " " + prezime + " (" + Godine() + ", " + jmbg + ")";
+        }
+    }
+}
---
+++ ProgramNeki/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramNeki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ovo je prva osoba:");
+            Osoba Prva = new Osoba("Dusan", "Popovic", "040700412345", 2004);
+            Console.WriteLine(Prva);
+            Console.ReadKey();
+        }
+    }
+}
